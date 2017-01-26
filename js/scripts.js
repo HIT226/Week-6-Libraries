d76f1d5b2f65aec22f4bbac96d5a792943c9c68f
Jason Carney <jason.jay.carney@gmail.com>
2017-01-26 13:35:45 +0930

    adding sweetalert and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,11 @@
 * email: {email}
 * website: {portfolio}
 */
+
+jQuery('.alert').on('click', function(e){
+	sweetAlert({
+	  title: "You clicked the button",
+	  text: "This is a message to let you know you clicked the button",
+	  type: "success"
+	});
+});
